c3a64bd51013a4679676dbe969f40df1d05a9112
George <george.nicolae86@yahoo.com>
2025-08-26 03:47:44 +0000

    Adaugat strings.py

---
+++ 250 exercices/strings.py
@@ -0,0 +1,26 @@
+un_sir = "Un test random care are si cifre 123 si cu spatii in plus   si cu diverse"
+
+print(f'{un_sir.index("n")} - afisam idexul unui caracter dintr-un string')
+print(f'{un_sir.count("t")} - numaram cate caractere sunt intr-un string')
+print(f'{un_sir.find("re")} - gasim grupul de caractere intr-un string')
+print(un_sir[1]) #afisam careacterul din string in functie de index
+
+
+
+
+'''
+Exercise 1 - Strings
+Given the code below, insert the correct negative index on line 3 in order to get the last character in the string.
+
+
+
+How to solve a coding exercise:
+
+Write your solution in the field below and then click on the Check Solution button.
+
+If the solution you provide is incorrect, then try to understand the error message that appears on the right side of the screen, re-write your solution and press the Check Solution button again.
+
+Finally, if you give up on this exercise and decide to move on, then click the Continue button.
+'''
+
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
