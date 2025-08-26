e930091f71ff5a5756ad895924eb8a34bf1f156a
George <george.nicolae86@yahoo.com>
2025-08-26 04:07:25 +0000

    Adaugare cod

--- 250 exercices/strings.py
+++ 250 exercices/strings.py
@@ -3,10 +3,11 @@ un_sir = "Un test random care are si cifre 123 si cu spatii in plus   si cu dive
 print(f'{un_sir.index("n")} - afisam idexul unui caracter dintr-un string')
 print(f'{un_sir.count("t")} - numaram cate caractere sunt intr-un string')
 print(f'{un_sir.find("re")} - gasim grupul de caractere intr-un string')
-print(un_sir[1]) #afisam careacterul din string in functie de index
-
-
-
+print(un_sir[1]) # afisam careacterul din string in functie de index
+print(un_sir.lower()) # afisam totul cu litere mici
+print(un_sir.upper()) # afisam totul cu litere mari
+print(un_sir.startswith("U")) # verificam daca un string incepe cu un anumit caracter
+print(un_sir.endswith("e")) # verificam daca un string se termina cu un anumit caracter
 
 '''
 Exercise 1 - Strings
